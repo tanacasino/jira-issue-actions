ce8d91d93cd255d8140a943e4bbc0681ed138859
Tomofumi Tanaka <tanacasino@gmail.com>
2020-09-23 21:39:50 +0900

    Tweak

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const wait = require('./wait');
 // most @actions toolkit packages have async methods
 async function run() {
   try {
+    const host = core.getInput('jira-api-host', {required: true});
+    const user = core.getInput('jira-api-user', {required: true});
+    const token = core.getInput('jira-api-token', {required: true});
+
+    const body = core.getInput('body');
+
     const ms = core.getInput('milliseconds');
     core.info(`Waiting ${ms} milliseconds ...`);
 
