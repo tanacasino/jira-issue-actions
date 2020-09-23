e867ed8d456cf43b3bf1ac8cda7357a47584876a
Tomofumi Tanaka <tanacasino@gmail.com>
2020-09-23 22:39:19 +0900

    fix test

--- index.test.js
+++ index.test.js
@@ -5,9 +5,9 @@ const dotenv = require('dotenv')
 
 test('test runs', () => {
   dotenv.config()
-  // process.env['INPUT_JIRA-API-HOST'] = 'dummy-host';
-  // process.env['INPUT_JIRA-API-USER'] = 'dummy-user';
-  // process.env['INPUT_JIRA-API-TOKEN'] = 'dummy-token';
+  process.env['INPUT_JIRA-API-HOST'] = process.env['INPUT_JIRA-API-HOST']  || 'dummy-host';
+  process.env['INPUT_JIRA-API-USER'] = process.env['INPUT_JIRA-API-USER'] ||  'dummy-user';
+  process.env['INPUT_JIRA-API-TOKEN'] = process.env['INPUT_JIRA-API-TOKEN']  || 'dummy-token';
   const body = {
     "fields": {
       "project": {
@@ -23,6 +23,8 @@ test('test runs', () => {
   }
   process.env['INPUT_BODY'] = JSON.stringify(body);
 
-  const ip = path.join(__dirname, 'index.js');
-  console.log(cp.execSync(`node ${ip}`, {env: process.env}).toString());
+  if (process.env['INPUT_JIRA-API-HOST']  !== 'dummy-host') {
+    const ip = path.join(__dirname, 'index.js');
+    console.log(cp.execSync(`node ${ip}`, {env: process.env}).toString());
+  }
 })
