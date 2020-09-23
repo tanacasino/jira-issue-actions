fe2821a2e7a9449c7501d359b3e3ffc9ce3e9e2a
Tomofumi Tanaka <tanacasino@gmail.com>
2020-09-23 22:28:35 +0900

    update

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const fetch = require('node-fetch');
 
 
-// most @actions toolkit packages have async methods
+function base64encode(value) {
+  const buffer = Buffer.from(value, 'ascii');
+  return buffer.toString('base64');
+}
+
+
 async function run() {
   try {
     const host = core.getInput('jira-api-host', {required: true});
     const user = core.getInput('jira-api-user', {required: true});
     const token = core.getInput('jira-api-token', {required: true});
-
     const body = core.getInput('body');
 
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const encodedToken = base64encode(`${user}:${token}`);
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    const params = {
+      method: 'POST',
+      body: body,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic ' + encodedToken,
+      }
+    }
 
-    core.setOutput('time', new Date().toTimeString());
+    const url = `${host}/rest/api/2/issue`;
+    const response = await fetch(url, params);
+    const json = await response.json();
+
+    core.setOutput('result', JSON.stringify(json));
   } catch (error) {
     core.setFailed(error.message);
+    console.error(error);
   }
 }
 
--- index.test.js
+++ index.test.js
@@ -1,23 +1,28 @@
-const wait = require('./wait');
 const process = require('process');
 const cp = require('child_process');
 const path = require('path');
+const dotenv = require('dotenv')
 
-test('throws invalid number', async () => {
-  await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
-});
-
-test('wait 500 ms', async () => {
-  const start = new Date();
-  await wait(500);
-  const end = new Date();
-  var delta = Math.abs(end - start);
-  expect(delta).toBeGreaterThanOrEqual(500);
-});
-
-// shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = 500;
+  dotenv.config()
+  // process.env['INPUT_JIRA-API-HOST'] = 'dummy-host';
+  // process.env['INPUT_JIRA-API-USER'] = 'dummy-user';
+  // process.env['INPUT_JIRA-API-TOKEN'] = 'dummy-token';
+  const body = {
+    "fields": {
+      "project": {
+        "id": "10000",
+      },
+      "summary": "Issue Summary",
+      "issuetype": {
+        "id": "10002"
+      },
+      "labels": ["apple"],
+      "customfield_10014": "ONE-2",
+    }
+  }
+  process.env['INPUT_BODY'] = JSON.stringify(body);
+
   const ip = path.join(__dirname, 'index.js');
   console.log(cp.execSync(`node ${ip}`, {env: process.env}).toString());
 })
